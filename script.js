4603ec23bad825d21c98a6105a8e07fad5aac0a1
Sam Limby <sam.limby@genesys.com>
2024-09-12 09:40:57 +0100

    changes to recent state and activation of the search dropdown when the input is hit at 2

--- script.js
+++ script.js
@@ -37,154 +37,155 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function filterAndDisplayResults() {
         const query = searchInput.value.toLowerCase().trim();
+        console.log(query.length);
 
-        if (query === "") {
+        if (query.length === 0) {
             frameContainer.innerHTML = originalContent;
             return;
-        }
-
-        // checking if help section already existings
-        let existingHelpBlock = document.getElementById("search-help_block");
-        if (!existingHelpBlock) {
-            const helpBlock = document.createElement("div");
-            helpBlock.setAttribute("id", "search-help_block")
-            helpBlock.innerHTML = `
-                <div class="help-block">
-                    <div class="help-block_nested">
-                        <div class="help-block_upper-container">
-                            <img src="icons/help-icon.png">
-                            <h3 class="help-title_text">Help</h3>
-                        </div>
-                        <p id="help-text_block" class="help-body_text">23 related articles to “${searchInput.value}” found in help. Click to explore.</p>
-                    </div>
-                    <img src="icons/external-link.png">
-                </div>
-            `;
-    
-            sideSheet.appendChild(helpBlock);
-        }
-
-        frameContainer.innerHTML = '';
-
-        const filteredResults = searchData.filter(item => {
-            console.log("a")
-            // Ensure title are defined before using them
-            const title = item.result_title ? item.result_title.toLowerCase() : " ";
-            return title.includes(query);
-        });
-
-        // containers for different categories & help below
-        peopleContainer.innerHTML = `
-            <div class="content-container_subtitle-row">
-                <img src="icons/user-icon.png">
-                <h3 class="content-subtitle_text">Directory</h3>
-                <div class="content-header_line"></div>
-                <img src="icons/kebab-menu.png">
-            </div>
-        `;
-        peopleContainer.classList.add("content-section");
-
-        pageContainer.innerHTML = `
-            <div class="content-container_subtitle-row">
-                <img src="icons/page-icon.png">
-                <h3 class="content-subtitle_text">Pages</h3>
-                <div class="content-header_line"></div>
-                <img src="icons/kebab-menu.png">
-            </div>
-        `;
-        pageContainer.classList.add("content-section");
-
-
-        if (filteredResults.length > 0) {
-            filteredResults.forEach(result => {
-                const inlineRow = document.createElement("div");
-                inlineRow.classList.add('inline-row');
-                frameContainer.classList.add('dynamic-content_block');
-
-                let innerHTMLContent = `
-                    <div class="inline-row">
-                `;
-
-                // specifying id and adding image to just people
-                if (result.category === "people") {
-                    inlineRow.setAttribute("id", "people-inlineRow");
-                    innerHTMLContent += `
-                        <img class='sm-avatar' src="images/${result.image}"
-                    `;
-                };
-
-                if (result.category === "page") {
-                    inlineRow.setAttribute("id", "page-inlineRow");
-                };
-                
-                // Updated HTML structure to include title, description and image
-                innerHTMLContent += `
-                        <div class="inline-row_nested">
-                            <div class="inline-text_container">
-                                <div class="upper_inline-text_container">
-                                    <h4 class="inline-title_text">${result.result_title}</h4>
-                `;
-
-                // Adding status to just people
-                if (result.category === "people") {
-                    innerHTMLContent += `
-                        <div class="inline-divider"></div>
-                        <div class="inline-activity_bar">
-                            <div class="active-circle"></div>
-                            <p class="inline-footnote_text">
-                                Active
-                            </p>
-                        </div>
-                    `;
-                };
-
-                innerHTMLContent += `
-                                </div>
-                                <div class="lower_inline-text_container">
-                                    <p class="inline-body_text">${result.role}</p>
-                                    <div class="inline-divider"></div>
-                                    <p class="inline-footnote_text">${result.team}</p>
-                                </div>
+        } else if (query.length === 2) {
+            // checking if help section already existings
+            let existingHelpBlock = document.getElementById("search-help_block");
+            if (!existingHelpBlock) {
+                const helpBlock = document.createElement("div");
+                helpBlock.setAttribute("id", "search-help_block")
+                helpBlock.innerHTML = `
+                    <div class="help-block">
+                        <div class="help-block_nested">
+                            <div class="help-block_upper-container">
+                                <img src="icons/help-icon.png">
+                                <h3 class="help-title_text">Help</h3>
                             </div>
+                            <p id="help-text_block" class="help-body_text">23 related articles to “${searchInput.value}” found in help. Click to explore.</p>
                         </div>
-                        <img src="icons/open.png">
+                        <img src="icons/external-link.png">
                     </div>
                 `;
+        
+                sideSheet.appendChild(helpBlock);
+            }
 
-                inlineRow.innerHTML = innerHTMLContent;
+            frameContainer.innerHTML = '';
 
-                allQuickFilter.innerHTML = `
-                    <img src="icons/checked.png">
-                    All
-                `;
-
-                if (result.category === "people") {
-                    peopleContainer.appendChild(inlineRow);
-                } else if (result.category === "page") {
-                    pageContainer.appendChild(inlineRow);
-                }
+            const filteredResults = searchData.filter(item => {
+                console.log("a")
+                // Ensure title are defined before using them
+                const title = item.result_title ? item.result_title.toLowerCase() : " ";
+                return title.includes(query);
             });
 
-            if (peopleContainer.hasChildNodes()) {
-                frameContainer.appendChild(peopleContainer);
-            }
-            if (pageContainer.hasChildNodes()) {
-                frameContainer.appendChild(pageContainer);
-            }
-        } else {
-            frameContainer.innerHTML = '<p class="inline-title_text>No results found</p>';
-        }
+            // containers for different categories & help below
+            peopleContainer.innerHTML = `
+                <div class="content-container_subtitle-row">
+                    <img src="icons/user-icon.png">
+                    <h3 class="content-subtitle_text">Directory</h3>
+                    <div class="content-header_line"></div>
+                    <img src="icons/kebab-menu.png">
+                </div>
+            `;
+            peopleContainer.classList.add("content-section");
 
-        //removing container when no associated results
-        if (!peopleContainer.querySelector(".inline-row")) {
-            peopleContainer.remove();
-        }
-        if (!pageContainer.querySelector(".inline-row")) {
-            pageContainer.remove();
-        }
+            pageContainer.innerHTML = `
+                <div class="content-container_subtitle-row">
+                    <img src="icons/page-icon.png">
+                    <h3 class="content-subtitle_text">Pages</h3>
+                    <div class="content-header_line"></div>
+                    <img src="icons/kebab-menu.png">
+                </div>
+            `;
+            pageContainer.classList.add("content-section");
 
-        let helpTextElement = document.getElementById("help-text_block");
-        helpTextElement.innerHTML = `<p id="help-text_block" class="help-body_text">23 related articles to “${searchInput.value}” found in help. Click to explore.</p>`
+
+            if (filteredResults.length > 0) {
+                filteredResults.forEach(result => {
+                    const inlineRow = document.createElement("div");
+                    inlineRow.classList.add('inline-row');
+                    frameContainer.classList.add('dynamic-content_block');
+
+                    let innerHTMLContent = `
+                        <div class="inline-row">
+                    `;
+
+                    // specifying id and adding image to just people
+                    if (result.category === "people") {
+                        inlineRow.setAttribute("id", "people-inlineRow");
+                        innerHTMLContent += `
+                            <img class='sm-avatar' src="images/${result.image}"
+                        `;
+                    };
+
+                    if (result.category === "page") {
+                        inlineRow.setAttribute("id", "page-inlineRow");
+                    };
+                    
+                    // Updated HTML structure to include title, description and image
+                    innerHTMLContent += `
+                            <div class="inline-row_nested">
+                                <div class="inline-text_container">
+                                    <div class="upper_inline-text_container">
+                                        <h4 class="inline-title_text">${result.result_title}</h4>
+                    `;
+
+                    // Adding status to just people
+                    if (result.category === "people") {
+                        innerHTMLContent += `
+                            <div class="inline-divider"></div>
+                            <div class="inline-activity_bar">
+                                <div class="active-circle"></div>
+                                <p class="inline-footnote_text">
+                                    Active
+                                </p>
+                            </div>
+                        `;
+                    };
+
+                    innerHTMLContent += `
+                                    </div>
+                                    <div class="lower_inline-text_container">
+                                        <p class="inline-body_text">${result.role}</p>
+                                        <div class="inline-divider"></div>
+                                        <p class="inline-footnote_text">${result.team}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <img src="icons/open.png">
+                        </div>
+                    `;
+
+                    inlineRow.innerHTML = innerHTMLContent;
+
+                    allQuickFilter.innerHTML = `
+                        <img src="icons/checked.png">
+                        All
+                    `;
+
+                    if (result.category === "people") {
+                        peopleContainer.appendChild(inlineRow);
+                    } else if (result.category === "page") {
+                        pageContainer.appendChild(inlineRow);
+                    }
+                });
+
+                if (peopleContainer.hasChildNodes()) {
+                    frameContainer.appendChild(peopleContainer);
+                }
+                if (pageContainer.hasChildNodes()) {
+                    frameContainer.appendChild(pageContainer);
+                }
+            } else {
+                frameContainer.innerHTML = '<p class="inline-title_text>No results found</p>';
+            }
+
+            //removing container when no associated results
+            if (!peopleContainer.querySelector(".inline-row")) {
+                peopleContainer.remove();
+            }
+            if (!pageContainer.querySelector(".inline-row")) {
+                pageContainer.remove();
+            }
+
+            let helpTextElement = document.getElementById("help-text_block");
+            helpTextElement.innerHTML = `<p id="help-text_block" class="help-body_text">23 related articles to “${searchInput.value}” found in help. Click to explore.</p>`
+        };
 
     };
 
